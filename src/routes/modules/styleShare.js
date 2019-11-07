f9b9f73425abfa9d668280f961420f9f96b340bf
肖彬 <>
2019-11-07 17:54:45 +0800

    feat(测款)：测款分享、测款报告页面

---
+++ src/routes/modules/styleShare.js
@@ -0,0 +1,10 @@
+export default {
+    path: '/testStyle/share',
+    name: 'styleShare',
+    meta: {
+      title: '分享测款',
+      keepAlive: true
+    },
+    component: () => import('@/views/testStyle/styleShare.vue')
+  }
+  
---
+++ src/views/common/shareList.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="panel">
+    <img
+      src="../../themes/images/app/vote_banner@2x.png"
+      alt=""
+    >
+    <div class="content">
+      <div class="top">
+        <p>这一季时尚货品，全听你的！</p>
+        <div class="raduis radu-Left"></div>
+        <div class="raduis radu-right"></div>
+      </div>
+      <div class="product-list">
+        <div
+          class="product"
+          v-for="(product,index) in productList"
+          :key="index"
+        >
+          <img
+            src="../../themes/images/app/vote_banner@2x.png"
+            alt=""
+          >
+          <p>{{product.title}}</p>
+          <button
+            :class="[product.isSelect? 'select-actived':'']"
+            @click="selected(product)"
+          >{{product.isSelect?"已选":"选TA"}}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    productList: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  methods: {
+    selected(product) {
+      this.$emit("selectClick", product);
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+.panel {
+  img {
+    width: 100%;
+  }
+  .content {
+    background-color: white;
+    .top {
+      background-color: white;
+      text-align: center;
+      border-radius: 8px;
+      height: 38px;
+      width: 100%;
+      z-index: 100;
+      transform: translateY(-15px);
+      p {
+        margin: 0;
+        line-height: 60px;
+        font-weight: bold;
+        // font-family: AlibabaPuHuiTiM;
+        font-size: 16px;
+        color: #2a2b33;
+      }
+      .raduis {
+        background-color: #3c5cf6;
+        width: 4px;
+        height: 4px;
+        border-radius: 2px;
+      }
+      .radu-Left {
+        transform: translate(10px, -52px);
+      }
+      .radu-right {
+        float: right;
+        transform: translate(-10px, -55px);
+      }
+    }
+    .product-list {
+      //   background-color: red;
+      display: flex;
+      flex-wrap: wrap;
+      //   justify-content: space-around;
+      padding: 0px 10px 20px;
+      .product {
+        margin: 0 5px 20px;
+        // background-color: blue;
+        text-align: center;
+        width: 30%;
+        // &:nth-child(3n) {
+        //   margin-right: 0;
+        // }
+        img {
+          width: 100%;
+          height: 143px;
+          border-radius: 4px;
+          object-fit: cover;
+        }
+        p {
+          margin: 10px 0;
+          font-family: PingFangSC;
+          font-size: 12px;
+          color: #2a2b33;
+          .ellipsis();
+        }
+        button {
+          width: 75px;
+          height: 32px;
+          border-radius: 17px;
+          background-color: #3c5cf6;
+          font-family: PingFangSC;
+          font-size: 14px;
+          font-weight: 500;
+          color: #ffffff;
+        }
+        .select-actived {
+          background-color: #b2b5c1;
+        }
+      }
+    }
+  }
+}
+</style>
