948d93d9f4d3f359829e9a54c21de035b1c8c916
penghuan <260452054@qq.com>
2019-11-15 18:47:48 +0800

    PGC 查看大图

--- src/views/news/detail/index.vue
+++ src/views/news/detail/index.vue
@@ -8,6 +8,22 @@
 </template>
 
 <script>
+window.onerror = function (err) {
+  console.log('window.onerror: ' + err)
+}
+/* 这段代码是固定的，必须要放到js中 */
+function setupWebViewJavascriptBridge (callback) {
+  // console.log(WebViewJavascriptBridge)
+  let WebViewJavascriptBridge = callback
+  if (window.WebViewJavascriptBridge) { return callback(WebViewJavascriptBridge) }
+  if (window.WVJBCallbacks) { return window.WVJBCallbacks.push(callback) }
+  window.WVJBCallbacks = [callback]
+  var WVJBIframe = document.createElement('iframe')
+  WVJBIframe.style.display = 'none'
+  WVJBIframe.src = 'wvjbscheme://__BRIDGE_LOADED__'
+  document.documentElement.appendChild(WVJBIframe)
+  setTimeout(function () { document.documentElement.removeChild(WVJBIframe) }, 0)
+}
 export default {
   components: {
   },
@@ -81,7 +97,32 @@ export default {
         if (isiOS) {
           // wrap.setAttribute("style", "margin-bottom:2rem")
           /* 与OC交互的所有JS方法都要放在此处注册，才能调用通过JS调用OC或者让OC调用这里的JS */
-          this.setupWebViewJavascriptBridge(imgs, i, bridgesData)
+          setupWebViewJavascriptBridge(function (bridge) {
+            /* Initialize your app here */
+
+            /* 我们在这注册一个js调用OC的方法，不带参数，且不用ObjC端反馈结果给JS：打开本demo对应的博文 */
+            bridge.registerHandler('openWebviewBridgeArticle', function () {
+              console.log('openWebviewBridgeArticle was called with by ObjC')
+            })
+            /* JS给ObjC提供公开的API，在ObjC端可以手动调用JS的这个API。接收ObjC传过来的参数，且可以回调ObjC */
+            bridge.registerHandler('getUserInfos', function (data, responseCallback) {
+              console.log('Get user information from ObjC: ', data)
+              responseCallback({ 'userId': '123456', 'blog': '' })
+            })
+
+            /* JS给ObjC提供公开的API，ObjC端通过注册，就可以在JS端调用此API时，得到回调。ObjC端可以在处理完成后，反馈给JS，这样写就是在载入页面完成时就先调用 */
+            bridge.callHandler('getUserIdFromObjC', function (responseData) {
+              console.log("JS call ObjC's getUserIdFromObjC function, and js received response:", responseData)
+            })
+
+            imgs[i].onclick = function (e) {
+              console.log('------callBackData', bridgesData)
+              bridge.callHandler('BridgeCallHandler', bridgesData, function (response) {
+                console.log('JS got response', response)
+              })
+            }
+            console.log('------callBackData', bridgesData)
+          })
         } else {
           console.log('------callBackData', bridgesData)
           imgs[i].onclick = function () {
@@ -151,32 +192,6 @@ export default {
         }
         n = i + 1
       }
-    },
-    setupWebViewJavascriptBridge (bridge, imgs, i, bridgesData) {
-      /* Initialize your app here */
-
-      /* 我们在这注册一个js调用OC的方法，不带参数，且不用ObjC端反馈结果给JS：打开本demo对应的博文 */
-      bridge.registerHandler('openWebviewBridgeArticle', function () {
-        console.log('openWebviewBridgeArticle was called with by ObjC')
-      })
-      /* JS给ObjC提供公开的API，在ObjC端可以手动调用JS的这个API。接收ObjC传过来的参数，且可以回调ObjC */
-      bridge.registerHandler('getUserInfos', function (data, responseCallback) {
-        console.log('Get user information from ObjC: ', data)
-        responseCallback({ 'userId': '123456', 'blog': '' })
-      })
-
-      /* JS给ObjC提供公开的API，ObjC端通过注册，就可以在JS端调用此API时，得到回调。ObjC端可以在处理完成后，反馈给JS，这样写就是在载入页面完成时就先调用 */
-      bridge.callHandler('getUserIdFromObjC', function (responseData) {
-        console.log("JS call ObjC's getUserIdFromObjC function, and js received response:", responseData)
-      })
-
-      imgs[i].onclick = function (e) {
-        console.log('------callBackData', bridgesData)
-        bridge.callHandler('BridgeCallHandler', bridgesData, function (response) {
-          console.log('JS got response', response)
-        })
-      }
-      console.log('------callBackData', bridgesData)
     },
     lazyload (n, num, img) { // 监听页面滚动事件
       let seeHeight = document.documentElement.clientHeight // 可见区域高度
