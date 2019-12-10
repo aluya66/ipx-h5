efc7e869cfbf9075eb8c9718ac236eb5e89adc45
肖彬 <>
2019-12-10 17:43:27 +0800

    fix()：ui修复

--- src/routes/modules/groupListDetail.js
+++ src/routes/modules/groupListDetail.js
@@ -3,7 +3,7 @@ export default {
     path: '/hall/groupListDetail',
     name: 'groupListDetail',
     meta: {
-        title: '组货清单详情',
+        title: '',
         keepAlive: true
     },
     component: () => import('@/views/user/hall/groupListDetail.vue')
