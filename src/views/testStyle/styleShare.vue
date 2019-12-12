f793a1573090ae886e994f4f697b92e92b2f4124
yangxingbang <xumenglan123>
2019-12-12 11:49:16 +0800

    修复代码合并错误

--- src/views/testStyle/styleShare.vue
+++ src/views/testStyle/styleShare.vue
@@ -32,164 +32,186 @@
 </template>
 
 <script>
-import shareList from '@/views/common/shareList'
-import utils from 'utils'
-export default {
-    components: {
-        shareList
-    },
-    data () {
-        return {
-            bannerCode: '',
-            participantCode: '',
-            bookActivityCode: '',
-            totalNum: '',
-            paddingTop: '',
-            list: [
-                {
-                    mainPic: '',
-                    productAtrNumber: '',
-                    productCode: ''
-                }
-            ]
-        }
-    },
-    activated () {
-        this.participantCode = this.$route.query.participantCode
-        this.bookActivityCode = this.$route.query.bookActivityCode
-        this.getSharemeasuresList()
+    import shareList from '@/views/common/shareList'
+    import utils from 'utils'
+    export default {
+        components: {
+            shareList
+        },
+        data () {
+            return {
+                bannerCode: '',
+                participantCode: '',
+                bookActivityCode: '',
+                totalNum: '',
+                paddingTop: '',
+                list: [
+                    {
+                        mainPic: '',
+                        productAtrNumber: '',
+                        productCode: ''
+                    }
+                ]
+            }
+        },
+        activated () {
+            // 上报页面事件
+            window.sa.track('IPX_WEB', {
+                page: 'styleShare',
+                type: 'pageView',
+                event: 'pageView'
+            })
+            this.participantCode = this.$route.query.participantCode
+            this.bookActivityCode = this.$route.query.bookActivityCode
+            this.getSharemeasuresList()
 
-        this.baseParams = utils.getStore('baseParams')
-        let statusBarHeight = this.baseParams.statusBarHeight || 0
-        let statusBarHeightSum = Number(statusBarHeight) / 100
-        if (this.baseParams.platform === 'ios') {
-            if (Number(this.baseParams.statusBarHeight) > 20) {
+            this.baseParams = utils.getStore('baseParams')
+            let statusBarHeight = this.baseParams.statusBarHeight || 0
+            let statusBarHeightSum = Number(statusBarHeight) / 100
+            if (this.baseParams.isIphoneX) {
                 this.paddingTop = (Number(statusBarHeightSum)) + 'rem'
             }
-        }
-    },
-    mounted () {
-        setTimeout(() => {
-            // console.log(document.querySelector('.van-pull-refresh'))
-
-            // console.log(this.$refs.shareList.offsetHeight)
-            // document.querySelector('.list-scroll').style.height =
-            //   (this.$refs.shareList.offsetHeight - 880) + 'px'
-            // if (this.$refs.shareList.offsetHeight > 3000) {
-            //   document.querySelector('.van-pull-refresh').style.height =
-            //   (this.$refs.shareList.offsetHeight - 860) + 'px'
-            // } else {
-            //   document.querySelector('.van-pull-refresh').style.height =
-            //   (this.$refs.shareList.offsetHeight - 650) + 'px'
-            // }
-
-            // document.querySelector('.van-list').style.height =
-            // (this.$refs.shareList.offsetHeight - 600) + 'px'
-            // document.querySelector('.van-pull-refresh__track').style.height =
-            // (this.$refs.shareList.offsetHeight - 1000) + 'px'
-        }, 500)
-    },
-    methods: {
-        getSharemeasuresList () {
-            const params = {
-                participantCode: this.participantCode
-            }
-            this.$api.book
-                .getSharemeasuresList(params)
-                .then(res => {
-                    this.list = res
-                    this.totalNum = res.length || 0
-                    if (this.list.length > 9) {
-                        this.list = this.list.splice(0, 9)
-                    }
-                })
-                .catch(err => {
-                    console.log(err)
-                })
         },
-        async bookShared () {
-            let params = {
-                participantCode: this.participantCode
-            }
-            await this.$api.book
-                .bookShared(params)
-                .then(res => {
-                    console.log(res)
-                })
-                .catch(err => {
-                    console.log(err)
-                })
+        mounted () {
+            setTimeout(() => {
+                // console.log(document.querySelector('.van-pull-refresh'))
+                //
+                // console.log(this.$refs.shareList.offsetHeight)
+                // // document.querySelector('.list-scroll').style.height =
+                // //   (this.$refs.shareList.offsetHeight - 880) + 'px'
+                // if (this.$refs.shareList.offsetHeight > 3000) {
+                //     document.querySelector('.van-pull-refresh').style.height =
+                //         (this.$refs.shareList.offsetHeight - 860) + 'px'
+                // } else {
+                //     document.querySelector('.van-pull-refresh').style.height =
+                //         (this.$refs.shareList.offsetHeight - 650) + 'px'
+                // }
+
+                // document.querySelector('.van-list').style.height =
+                // (this.$refs.shareList.offsetHeight - 600) + 'px'
+                // document.querySelector('.van-pull-refresh__track').style.height =
+                // (this.$refs.shareList.offsetHeight - 1000) + 'px'
+            }, 500)
         },
-        async shareWechat (type) {
-            // let url = window.location.host
-            let url = 'h5.yosar.com'
-            // type 1=好友 2=朋友圈
-            let method = 'one_key_share'
-            let params = {
-                type: String(type),
-                title: '我想邀请你一起做时尚买手',
-                url: `https://${url}/ipxhybrid/oauth?bookActivityCode=${this.bookActivityCode}&participantCode=${this.participantCode}`,
-                // shareImage: 'http://media.yosar.com/19/324/1574152045660',
-                description: '这一季时尚选款，就听你的！为你偏爱的原创款式代言！'
+        methods: {
+            getSharemeasuresList () {
+                const params = {
+                    participantCode: this.participantCode
+                }
+                this.$api.book
+                    .getSharemeasuresList(params)
+                    .then(res => {
+                        this.list = res
+                        this.totalNum = res.length || 0
+                        if (this.list.length > 9) {
+                            this.list = this.list.splice(0, 9)
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            },
+            async bookShared () {
+                let params = {
+                    participantCode: this.participantCode
+                }
+                await this.$api.book
+                    .bookShared(params)
+                    .then(res => {
+                        console.log(res)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    })
+            },
+            async shareWechat (type) {
+                // type 1=好友
+                if (type === 1) {
+                    // 上报按钮事件
+                    window.sa.track('IPX_WEB', {
+                        page: 'styleShare',
+                        type: 'click',
+                        event: 'shareGoodFriends'
+                    })
+                } else {
+                    // 2=朋友圈
+                    // 上报按钮事件
+                    window.sa.track('IPX_WEB', {
+                        page: 'styleShare',
+                        type: 'click',
+                        event: 'shareWechatMoments'
+                    })
+                }
+                // let url = window.location.host
+                let url = 'h5.yosar.com'
+                // type 1=好友 2=朋友圈
+                let method = 'one_key_share'
+                let params = {
+                    type: String(type),
+                    title: '我想邀请你一起做时尚买手',
+                    url: `https://${url}/ipxhybrid/oauth?bookActivityCode=${this.bookActivityCode}&participantCode=${this.participantCode}`,
+                    // shareImage: 'http://media.yosar.com/19/324/1574152045660',
+                    description: '这一季时尚选款，就听你的！为你偏爱的原创款式代言！'
+                }
+
+                await this.bookShared()
+
+                console.log(JSON.stringify(params) + 'params')
+
+                utils.postMessage(method, params)
             }
-
-            await this.bookShared()
-
-            console.log(JSON.stringify(params) + 'params')
-
-            utils.postMessage(method, params)
         }
     }
-}
 </script>
 
 <style lang="less" scoped>
-.share-panel{
-  // height: calc(89vh);
-  // overflow: auto;
-}
-.panel {
-  background-color: @color-c8;
-
-  .top-title {
-    width: 100%;
-    background-color: white;
-    padding: 12px 16px;
-    p {
-      margin: 0;
-      // font-family: PingFangSC;
-      font-size: 14px;
-      font-weight: 600;
-      color: #2a2b33;
-      line-height: 1.43;
-    }
-  }
-  .list-scroll {
-    overflow: hidden;
-    // height: calc(100vh - 200px);
-  }
-  .scale-content {
-    width: 120%;
-    margin: 16px 0;
-    border-radius: 12px;
-    border: solid 0.5px #e1e2e6;
-    background-color: white;
-    transform: scale(0.5, 0.5) translateX(-15%);
-    transform-origin: top;
-    padding: 10px;
-    height: calc(115vh);
+  .share-panel{
+    // height: calc(89vh);
     // overflow: auto;
-    &::-webkit-scrollbar {
-      display: none;
-    }
   }
-  .share-list{
-    height: calc(110vh);
-    overflow: auto;
-  }
-}
+  .panel {
+    background-color: @color-c8;
 
-.bottom-dialog {
+    .top-title {
+      width: 100%;
+      background-color: white;
+      padding: 12px 16px;
+      p {
+        margin: 0;
+        // font-family: PingFangSC;
+        font-size: 14px;
+        font-weight: 600;
+        color: #2a2b33;
+        line-height: 1.43;
+      }
+    }
+    .list-scroll {
+      overflow: hidden;
+      // height: calc(100vh - 200px);
+    }
+    .scale-content {
+      width: 120%;
+      margin: 16px 0;
+      border-radius: 12px;
+      border: solid 0.5px #e1e2e6;
+      background-color: white;
+      transform: scale(0.5, 0.5) translateX(-15%);
+      transform-origin: top;
+      padding: 10px;
+      height: calc(115vh);
+      // overflow: auto;
+      &::-webkit-scrollbar {
+        display: none;
+      }
+    }
+    .share-list{
+      height: calc(110vh);
+      overflow: auto;
+    }
+
+  }
+
+  .bottom-dialog {
     position: fixed;
     bottom: 0;
     background-color: white;
