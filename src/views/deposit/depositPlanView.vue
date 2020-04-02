80bf387b98439eeda3d3c4f514acd69c8444de08
Shamo <Sun.Shamo@foxmail.com>
2020-04-02 16:16:03 +0800

    1、极速上店最新UI修改

--- src/views/deposit/depositPlanView.vue
+++ src/views/deposit/depositPlanView.vue
@@ -1,10 +1,34 @@
 <template>
     <div class="plan-contain">
         <div class="plan-shadow">
-            <section class="plan-header">
-                <p class="plan-title">{{data.remark}}</p>
-            </section>
-            <div class="plan-moneyContain">
+            <div class="plan-header-con">
+                <div class="plan-header">
+                    <p class="plan-title">{{data.remark}}：付{{data.depositAmount}}押金</p>
+                </div>
+            </div>
+            <div class="plan-conditions">
+                <div class="condition">
+                    <img src="../../themes/images/icon_consumption_quota@2x.png" />
+                    <div class="data"><span class="number">{{data.consumeAmount}}</span><span class="unit">元</span></div>
+                    <span>消费额度</span>
+                </div>
+                <div class="condition">
+                    <img src="../../themes/images/icon_validity_period@2x.png" />
+                    <div class="data"><span class="number">{{data.effectiveDays}}</span><span class="unit">天</span></div>
+                    <span>押金有效期</span>
+                </div>
+                <div class="condition">
+                    <img src="../../themes/images/icon_returns@2x.png" />
+                    <div class="data"><span class="number">{{data.refundTimes}}</span><span class="unit">次</span></div>
+                    <span>免费全场退货</span>
+                </div>
+                <div class="condition">
+                    <img src="../../themes/images/icon_replacement_ratio@2x.png" />
+                    <div class="data"><span class="number">{{data.refundRate}}</span><span class="unit">%</span></div>
+                    <span>退换货比例</span>
+                </div>
+            </div>
+            <!--<div class="plan-moneyContain">
                 <p class="plan-money">{{data.depositAmount}}<span> 元押金</span></p>
                 <p style="padding:0 0.1rem">=</p>
                 <p class="plan-money">{{data.consumeAmount}}<span> 元消费额度</span></p>
@@ -13,14 +37,17 @@
                 <p>{{data.effectiveDays}}天押金有效期</p>
                 <p>{{data.refundTimes}}天退货周期</p>
                 <p>{{data.refundRate}}%受理退货</p>
+            </div>-->
+            <div class="open-vip" @click.stop="goToBuy(data.depositConfigId)">
+                开通享权益
             </div>
         </div>
-        <div class="out-side-left" v-show="showConnectIcon">
+        <!--<div class="out-side-left" v-show="showConnectIcon">
             <img :src="connetImg" alt="">
         </div>
         <div class="out-side-right" v-show="showConnectIcon">
             <img :src="connetImg" alt="">
-        </div>
+        </div>-->
     </div>
 </template>
 
@@ -45,6 +72,16 @@ export default {
     data() {
         return {
             connetImg: require('@/themes/images/app/icon_connect@3x.png')
+        }
+    },
+    methods: {
+        goToBuy(depositConfigId) {
+            this.$router.push({
+                path: '/deposit/purchasePlan',
+                query: {
+                    depositConfigId: depositConfigId
+                }
+            })
         }
     }
 }
@@ -76,39 +113,90 @@ export default {
     }
 }
 .plan-shadow {
-    box-shadow:0px 3px 20px 0px rgba(33,44,98,0.12);
-    background: #fff;
+    text-align: center;
+    box-shadow:0 3px 20px 0 rgba(33,44,98,0.12);
+    background: url("../../themes/images/bg_deposit_plan.png") no-repeat;
+    background-size: 100%;
     border-radius: 12px;
-    overflow: hidden;
+    margin-top: 8px;
+    padding-bottom: 32px;
+}
+.plan-conditions {
+    display: flex;
+    flex-direction: row;
+    margin-top: 28px;
+    .condition {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        flex: 1;
+        >img {
+            width: 44px;
+            height: 44px;
+        }
+        .data {
+            color: @color-c1;
+            .number {
+                font-size: @f18;
+                font-weight: bold;
+                font-family: 'alibabaBold';
+                line-height: 22px;
+            }
+            .unit {
+                font-size: 10px;
+                line-height: 16px;
+            }
+        }
+        >span {
+            color: @color-c3;
+            font-size: 12px;
+            margin-top: 4px;
+        }
+    }
+}
+.open-vip {
+    color: white;
+    margin: 28px 25px 0 25px;
+    line-height: 50px;
+    text-align: center;
+    background:linear-gradient(135deg,rgba(85,122,244,1) 0%,rgba(114,79,255,1) 100%);
+    border-radius: 25px;
+    font-size: 18px;
 }
 .plan-contain {
-    padding: 2px 0px 10px;
+    padding-bottom: 24px;
     background: #fff;
     position: relative;
-    .plan-header {
-        height:46px;
-        background:linear-gradient(135deg,rgba(85,122,244,1) 0%,rgba(114,79,255,1) 100%);
-        text-align: center;
-        .plan-title {
-            display: inline;
-            margin: 0 auto;
+    .plan-header-con {
+        display: flex;
+        justify-content: center;
+        .plan-header {
+            width: 235px;
+            height:40px;
+            background: url("../../themes/images/bg_title_deposit@2x.png") no-repeat;
+            background-size: 100%;
             text-align: center;
-            line-height: 46px;
-            font-size:16px;
-            font-family:PingFangSC-Medium,PingFang SC;
-            font-weight:500;
-            color:rgba(255,255,255,1);
-            position: relative;
-            &::after {
-                content: '';
-                height:2px;
-                background:rgba(255,255,255,1);
-                border-radius:2px;
-                opacity:0.1;
-                position: absolute;
-                bottom: 1px;
-                left: 0;
-                width: 100%;
+            margin-top: -8px;
+            .plan-title {
+                margin-top: -8px;
+                display: inline;
+                text-align: center;
+                line-height: 46px;
+                font-size:16px;
+                font-weight:bold;
+                color:rgba(68,70,66,1);
+                position: relative;
+                &::after {
+                    content: '';
+                    height:2px;
+                    background:rgba(255,255,255,1);
+                    border-radius:2px;
+                    opacity:0.1;
+                    position: absolute;
+                    bottom: 1px;
+                    left: 0;
+                    width: 100%;
+                }
             }
         }
     }
--- src/views/deposit/purchasePlanView.vue
+++ src/views/deposit/purchasePlanView.vue
@@ -4,14 +4,14 @@
             <div slot="title">请选择购买方案</div>
         </c-header>
         <div class="plan-list" :style="getBottomOffset(84)">
-            <div :class="['item-shape', selectIndex===key?'item-shape-select':'']" v-for="(item, key) in plans"
-                 :key="key" v-on:click="selectIndex=key">
-                <div :class="['item-title', selectIndex===key?'item-title-select':'']">
+            <div :class="['item-shape', depositConfigId===item.depositConfigId?'item-shape-select':'']" v-for="(item, key) in plans"
+                 :key="key" @click="selectPlan(item.depositConfigId)">
+                <div :class="['item-title', depositConfigId===item.depositConfigId?'item-title-select':'']">
                     <span class="text-size-18">{{item.remark}}：</span><span class="text-size-16">付</span><span
                     class="text-size-24">{{item.depositAmount}}</span><span
                     class="text-size-16">元押金</span>
                 </div>
-                <div :class="['item-content', selectIndex===key?'item-content-select':'']">
+                <div :class="['item-content', depositConfigId===item.depositConfigId?'item-content-select':'']">
                     <p>{{item.consumeAmount}}元消费额度</p>
                     <p>{{item.effectiveDays}}天押金有效期</p>
                     <p>{{item.refundTimes}}天退货周期</p>
@@ -42,8 +42,8 @@ export default {
         return {
             isNative: false,
             plans: [],
-            selectIndex: 0,
-            userData: {}
+            userData: {},
+            depositConfigId: 1
         }
     },
     methods: {
@@ -68,8 +68,8 @@ export default {
 
             })
         },
-        goPay() {
-            const item = this.plans[this.selectIndex]
+        goPay(index) {
+            const item = this.plans[index]
             const params = {
                 jumpUrl: 'depositPayWay://',
                 depositConfigId: item.depositConfigId === undefined ? '' : item.depositConfigId + '', // 押金配置id
@@ -77,11 +77,17 @@ export default {
                 consumeAmount: item.consumeAmount + '' // 消费额度
             }
             utils.postMessage('', params)
+        },
+        selectPlan(depositConfigId) {
+            this.depositConfigId = depositConfigId
         }
     },
     activated() {
         if (this.$route.query.fromNative === '1') {
             this.isNative = true
+        }
+        if (this.$route.query.depositConfigId !== undefined) {
+            this.depositConfigId = this.$route.query.depositConfigId
         }
         this.handleRequest()
         this.getAccountInfo()
