a1abb4c19dbfcb460fa1d3e91ac9c9c6b934e795
Shamo <Sun.Shamo@foxmail.com>
2020-04-22 16:51:57 +0800

    1、编辑组货滑动删除报错，取消删除后数据恢复；

--- src/views/user/hall/groupListDetail.vue
+++ src/views/user/hall/groupListDetail.vue
@@ -404,12 +404,6 @@ export default {
                 confirmButtonColor: '#007AFF'
             }).then(() => {
                 this.skuCommit()
-            }).catch(() => {
-                this.groupGoodsRecords.forEach(item => {
-                    if (item) {
-                        item.isSelected = false
-                    }
-                })
             })
         },
         jumpToProduct(product) {
